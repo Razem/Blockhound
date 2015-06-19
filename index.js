c847c620c8080cf724ed410057c34119d9755dee
Radomír Žemlička <info@razem.cz>
2015-06-19 14:14:58 +0200

    Logger, initial state

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 'use strict';
 var
 config = require("./config.json"),
+dirs = require("./lib/dirs.json"),
 FS = require("fs"),
 Path = require("path"),
 Stats = require("./lib/stats"),
 Player = require("./lib/player"),
+Logger = require("./lib/logger"),
 ReadLine = require("readline");
 
-// Just for test
-
-var last = Date.now();
-var watcher = FS.watch(Path.join(config.worldPath, "stats"), function (event, fileName) {
-  var now = Date.now();
-  console.log(now, now - last, event, fileName);
-
-  last = now;
-});
+Logger.init();
 
 // Command processing
 
@@ -27,7 +21,7 @@ var rl = ReadLine.createInterface({
 (function processCommands() {
   rl.question("", function (cmd) {
     if (cmd === "exit") {
-//      watcher.close();
+      Logger.stopWatcher();
       process.exit(0);
       return;
     }
