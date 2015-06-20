6fd37e3c074ec5f4cbcffda27d72fa9398c03b2c
Martin Brychta <martin@brychta.name>
2015-06-20 12:50:10 +0200

    FIlter prep; jQueryUI

--- ui/index.php
+++ ui/index.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <?php
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL | E_STRICT);
+//ini_set('display_errors', 1);
 require_once './functions.class.php';
 ?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="author" content="Martin Brychta, martin@brychta.name, http://brychta.name" />
+        <title>[Blockhound]</title>
+        
         <link rel="stylesheet" type="text/css" href="css.css" />
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <title>Blockhound</title>
+        <link rel="stylesheet" type="text/css" href="./jqueryui/jquery-ui.css" />
+        <link rel="stylesheet" type="text/css" href="./jqueryui/jquery-ui-timepicker-addon.css" />
+        
+        <script type="text/javascript" src="./jqueryui/external/jquery/jquery.js"></script>
+        <script type="text/javascript" src="./jqueryui/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="./jqueryui/jquery-ui-timepicker-addon.js"></script>
         <script type="text/javascript">
             $(document).ready(function () {
                 var tmp = [];
@@ -32,6 +38,14 @@ require_once './functions.class.php';
                 $("#changeNext").val(next);
                 $("#countPrev").val(values[1]);
                 $("#countNext").val(values[1]);
+
+                $(".button").button();
+                $(".date").datetimepicker({
+                    controlType: "select",
+                    oneLine: true,
+                    dateFormat: "yy-mm-dd",
+                    timeFormat: "HH:mm:ss"
+                });
             });
         </script>
     </head>
@@ -40,9 +54,13 @@ require_once './functions.class.php';
         <div id="outside">
             <div id="inside">
                 <form action="index.php" method="GET">
-                    START: <input type="text" name="start" value="0" /><br />
-                    COUNT: <input type="text" name="count" value="20" /><br />
-                    <input type="submit" name="submit" value="Submit"/>
+                    START :<input type="text" name="start" value="0" /><br />
+                    COUNT :<input type="text" name="count" value="20" /><br />
+                    NAME &nbsp;:<input type="text" name="name" /><br />
+                    DATE &nbsp;:<input class="date" type="text" name="date" /><br />
+                    COORD :<input class="coord" type="text" name="X" placeholder="X"><input class="coord" type="text" name="Y" placeholder="Y"><input class="coord" type="text" name="Z" placeholder="Z"><br />
+                    RADIUS:<input type="text" name="radius"><br />
+                    <input class="button" type="submit" name="submit" value="Submit"/>
                 </form>
                 <table>
                     <thead>
@@ -68,12 +86,12 @@ require_once './functions.class.php';
                 <form id="prev" action="index.php" method="GET">
                     <input id="changePrev" type="text" name="start" value="" style="display: none;"/>
                     <input id="countPrev" type="text" name="count" value="" style="display: none;"/>
-                    <input type="submit" name="submit" value="Prev"/>
+                    <input class="button" type="submit" name="submit" value="Prev"/>
                 </form>
                 <form id="next" action="index.php" method="GET">
                     <input id="changeNext" type="text" name="start" value="" style="display: none;"/>
                     <input id="countNext" type="text" name="count" value="" style="display: none;"/>
-                    <input type="submit" name="submit" value="Next"/>
+                    <input class="button" type="submit" name="submit" value="Next"/>
                 </form>
             </div>
         </div>
