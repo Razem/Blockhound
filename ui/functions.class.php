f3d308558ee97ab945313d7d0fe98a6c40b90c8d
Martin Brychta <martin@brychta.name>
2015-06-20 03:02:20 +0200

    UUIDs to Name parser with local weekly caching. Prep for paging

--- ui/functions.class.php
+++ ui/functions.class.php
@@ -19,8 +19,7 @@ class functions {
     }
 
     public function getRecords($start, $count) {
-        $result = $this->sql->query("SELECT * FROM blockhound_actions ORDER BY id DESC LIMIT ".intval($start).",".intval($count));
-        echo $this->sql->error;
+        $result = $this->sql->query("SELECT * FROM blockhound_actions ORDER BY id DESC LIMIT " . intval($start) . "," . intval($count));
         $html = "";
         while ($row = $result->fetch_array(MYSQLI_NUM)) {
             $world_type = intval($row[3]);
@@ -31,9 +30,26 @@ class functions {
             } else {
                 $world_type = "the Overworld";
             }
-            $html .= "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$world_type</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td><td>$row[7]</td><td>$row[8]</td></tr>";
+            $html .= "<tr><td>$row[0]</td><td>$row[1]</td><td>" . $this->getName($row[2]) . "</td><td>$world_type</td><td>$row[4] | $row[5] | $row[6]</td><td>$row[7]</td><td>$row[8]</td></tr>";
         }
         return $html;
+    }
+
+    public function getName($uuid) {
+        $result = $this->sql->query("SELECT * FROM blockhound_players_names WHERE uuid='" . $uuid . "'");
+        $now = date("Y-m-d H:i:s");
+        if ($result != null) {
+            $row = $result->fetch_array(MYSQLI_NUM);
+            if (strtotime($now) - strtotime($row[2]) < 604800) {
+                return $row[1];
+            } else {
+                $this->sql->query("DELETE FROM blockhound_players_names WHERE uuid = '" . $uuid . "'");
+            }
+        }
+        $json = file_get_contents("https://sessionserver.mojang.com/session/minecraft/profile/" . $uuid);
+        $profile = json_decode($json, true);
+        $this->sql->query("INSERT INTO blockhound_players_names(uuid, name, date_cached) VALUES ('" . $uuid . "', '" . $profile["name"] . "', '" . $now . "')");
+        return "<span style='color: #99ff99;'> " . $profile["name"] . "</span>";
     }
 
 }
--- ui/index.php
+++ ui/index.php
@@ -9,7 +9,18 @@ require_once './functions.class.php';
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="author" content="Martin Brychta, martin@brychta.name, http://brychta.name" />
         <link rel="stylesheet" type="text/css" href="css.css" />
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <title>Blockhound</title>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var id = $("#queries tr:first td:first").value;
+                var count = $("#queries ").children().length;
+                $("#countNext").value = count;
+                $("#countPrev").value = count;
+                $("#changeNext").value = id - 1;
+                $("#changePrev").value = id + count.value + 1;
+            });
+        </script>
     </head>
 
     <body>
@@ -18,13 +29,13 @@ require_once './functions.class.php';
                 <form action="index.php" method="GET">
                     START: <input type="text" name="start" value="0" /><br />
                     COUNT: <input type="text" name="count" value="20" /><br />
-                    <input type="submit" name="submit"/>
+                    <input type="submit" name="submit" value="Submit"/>
                 </form>
                 <table>
                     <thead>
-                    <td>id</td><td>date</td><td>uuid</td><td>world type</td><td>X</td><td>Y</td><td>Z</td><td>action name</td><td>action value</td>
+                    <td>ID</td><td>Date</td><td>Name</td><td>World</td><td>Coordinations</td><td>Action</td><td>Count</td>
                     </thead>
-                    <tbody>
+                    <tbody id="queries">
                         <?php
                         if (isset($_GET["submit"])) {
                             $start = 0;
@@ -41,6 +52,16 @@ require_once './functions.class.php';
                         ?>
                     </tbody>
                 </table>
+                <form action="index.php" method="GET">
+                    <input id="changePrev" type="text" name="start" value="0" style="display: none;"/>
+                    <input id="countPrev" type="text" name="count" value="0" style="display: none;"/>
+                    <input id="prev" type="submit" name="submit-prev" value="Prev"/>
+                </form>
+                <form action="index.php" method="GET">
+                    <input id="changeNext" type="text" name="start" value="0" style="display: none;"/>
+                    <input id="countNext" type="text" name="count" value="0" style="display: none;"/>
+                    <input id="next" type="submit" name="submit-next" value="Next"/>
+                </form>
             </div>
         </div>
     </body>
